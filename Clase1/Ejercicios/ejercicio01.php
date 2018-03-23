2c0f9e0e22e6fe9044a2d5325a82941344035aad
MarinaMeza <kaiodelnorte@gmail.com>
2018-03-23 19:21:37 -0300

    Ejercio 1 terminado

--- Clase1/Ejercicios/ejercicio01.php
+++ Clase1/Ejercicios/ejercicio01.php
@@ -1,5 +1,15 @@
 <?php
+	
+	$acum = 0;
 
-    for($i = 1; $acum <= 1001; $i++){}
+    for($i = 1; ; $i++){
+    	$acum += $i;
+    	if($acum >= 1000)
+    	{
+    		$acum -= $i;
+    		break;
+    	}
+    }
 
+    echo $acum;
 ?>
