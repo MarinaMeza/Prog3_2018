78a19ab8f1dd0a2e133da5d1c54f60ab6af588a5
MarinaMeza <kaiodelnorte@gmail.com>
2018-04-14 13:07:01 -0300

    Ejercicio 13 terminado

---
+++ Clase2/Ejercicios/ejercicio13.php
@@ -0,0 +1,17 @@
+<?php
+
+    $respuesta = '';
+    $palabra = $_POST["palabra"];
+    $max = $_POST["max"];
+
+    if (strlen($palabra) > $max) {
+        $respuesta = "La palabra es demasiado larga";
+    }elseif ( (strnatcasecmp($palabra,"Recuperatorio")) == 0 ||
+    (strnatcasecmp($palabra,"Parcial")) == 0 ||
+    (strnatcasecmp($palabra,"Programacion")) == 0 ) {
+        $respuesta = 1;
+    }else {
+        $respuesta = 0;
+    }
+    echo $respuesta;
+?>
